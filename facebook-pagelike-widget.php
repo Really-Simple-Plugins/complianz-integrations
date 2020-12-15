08e05b395724ea7463658b17cd35fcc1e2db494a
AertHulsebos <39100908+AertHulsebos@users.noreply.github.com>
2020-12-15 12:01:32 +0100

    Facebook Page Like Widget
    https://wordpress.org/plugins/facebook-pagelike-widget/

---
+++ facebook-pagelike-widget.php
@@ -0,0 +1,39 @@
+<?php
+defined( 'ABSPATH' ) or die( "you do not have acces to this page!" );
+add_filter( 'cmplz_known_script_tags', 'cmplz_fb_page_like_widget_script' );
+function cmplz_fb_page_like_widget_script( $tags ) {
+	$tags[] = 'fb.js';
+
+	return $tags;
+}
+
+
+/**
+ * Add some custom css for the placeholder
+ */
+
+add_action( 'wp_footer', 'cmplz_fb_pagelike_widget_css' );
+function cmplz_fb_pagelike_widget_css() {
+	?>
+	<style>
+		.fb_loader {
+			display:none;
+		}
+	</style>
+	<?php
+}
+
+
+function cmplz_fb_pagelike_widget_js() {
+	?>
+	<script>
+		jQuery(document).ready(function ($) {
+			$(document).on("cmplzRunAfterAllScripts", cmplz_fb_pagelike_widget_js);
+			function cmplz_fb_pagelike_widget_js() {
+				$(document).trigger("ready");
+			}
+		})
+	</script>
+	<?php
+}
+add_action( 'wp_footer', 'cmplz_fb_pagelike_widget_js' );

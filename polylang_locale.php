df1997a9400f8fc57ba895fc267728b55619091d
Rogier Lankhorst <rogier@really-simple-plugins.com>
2020-12-20 15:06:33 +0100

    code style

--- polylang_locale.php
+++ polylang_locale.php
@@ -5,6 +5,6 @@
  */
 add_filter('cmplz_cookiebanner_settings', 'cmplz_language_cookiebanner_settings', 10, 1);
 function cmplz_language_cookiebanner_settings($settings) {
-	$settings['url'] = add_query_arg('lang',  substr(get_locale(), 0, 2), admin_url( 'admin-ajax.php' ) );
+	$settings['url'] = add_query_arg('lang',  substr( get_locale(), 0, 2 ), admin_url( 'admin-ajax.php' ) );
 	return $settings;
 }

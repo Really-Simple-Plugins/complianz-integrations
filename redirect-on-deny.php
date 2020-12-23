2ba2fe559083fe02ce5a7f5fc8fe2de4219788fa
Rogier Lankhorst <rogier@really-simple-plugins.com>
2020-12-23 15:11:16 +0100

    redirect on deny

---
+++ redirect-on-deny.php
@@ -0,0 +1,15 @@
+<?php
+function cmplz_redirect_on_deny() {
+	?>
+	<script>
+        jQuery(document).ready(function ($) {
+            document.addEventListener('cmplzStatusChange', function (e) {
+                if (e.detail.category !== 'marketing') {
+                    window.location.href = "https://wordpress.org/plugins/complianz-gdpr/";
+                }
+            });
+        });
+	</script>
+	<?php
+}
+add_action( 'wp_footer', 'cmplz_redirect_on_deny' );

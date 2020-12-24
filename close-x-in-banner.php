16b820af85144eab6e563fe6738ce7683034794a
AertHulsebos <39100908+AertHulsebos@users.noreply.github.com>
2020-12-24 13:11:35 +0100

    Update close-x-in-banner.php

--- close-x-in-banner.php
+++ close-x-in-banner.php
@@ -1,21 +1,22 @@
 <?php
-
+/* Read our article on complianz.io/changing-dismiss-to-close-button/ for implementation */
 function cmplz_reload_after_consent() {
 	?>
 	<style>
         #cc-window .cc-compliance .cc-btn.cc-dismiss {
             position: absolute;
-            top: 5px;
-            right: -18px;
+            top: -15px;
+            right: 15px;
             text-align: right;
             margin-right: 30px;
             background-color: initial !important;
             border: 0;
             text-decoration: none;
         }
-        #cc-window .cc-compliance .cc-btn.cc-dismiss::before {
-            content: "x";
-        }
+/* Specifically for the Accept All + View Preferences Template */		
+	#cc-window .cc-save {
+	width:100%!important;
+}
 	</style>
 	<?php
 }

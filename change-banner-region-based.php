c3fc441679b408245649e0b53df2d791a02bf678
Rogier Lankhorst <rogier@really-simple-plugins.com>
2021-01-05 09:49:29 +0100

    consentype based

--- change-banner-region-based.php
+++ change-banner-region-based.php
@@ -1,5 +1,26 @@
 <?php
 /**
+ * Only one of the below functions should be used, don't use both at the same time!
+ */
+
+/**
+ * Change the banner configuration based on the consenttype.
+ * @param array $data
+ *
+ * @return array
+ */
+function cmplz_change_banner_consenttype_based( $data ) {
+	if (COMPLIANZ::$geoip->geoip_enabled()) {
+		$consenttype      = COMPLIANZ::$geoip->consenttype();
+		if ( $consenttype === 'optout' ) {
+			$data['position'] = 'bottom';
+		}
+	}
+	return $data;
+}
+add_filter('cmplz_ajax_loaded_banner_data', 'cmplz_change_banner_consenttype_based');
+
+/**
  * Change the banner configuration based on the region.
  * @param array $data
  *
@@ -15,3 +36,5 @@ function cmplz_change_banner_region_based( $data ) {
 	return $data;
 }
 add_filter('cmplz_ajax_loaded_banner_data', 'cmplz_change_banner_region_based');
+
+

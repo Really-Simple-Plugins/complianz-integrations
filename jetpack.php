90702c62c228ec12dddcce7e962815763a46844b
Rogier Lankhorst <rogier@really-simple-plugins.com>
2021-01-21 19:17:57 +0100

    f names

--- jetpack.php
+++ jetpack.php
@@ -19,13 +19,13 @@ function cmplz_jetpack_stats_script( $tags ) {
  * Make sure it's set as not anonymous when tracking enabled
  * @param bool $stats_category_required
  */
-function cmplz_jetpack_set_statistics_required( $stats_category_required ){
-	if ( class_exists( 'Jetpack' ) && !Jetpack::is_module_active( 'stats' ) ) {
+function cmplz_jetpack_stats_set_statistics_required( $stats_category_required ){
+	if ( class_exists( 'Jetpack' ) && Jetpack::is_module_active( 'stats' ) ) {
 		$stats_category_required = true;
 	}
 	return $stats_category_required;
 }
-add_filter('cmplz_cookie_warning_required_stats', 'cmplz_jetpack_set_statistics_required');
+add_filter('cmplz_cookie_warning_required_stats', 'cmplz_jetpack_stats_set_statistics_required');
 
 
 /**
@@ -34,11 +34,11 @@ add_filter('cmplz_cookie_warning_required_stats', 'cmplz_jetpack_set_statistics_
  *
  * @return array
  */
-function cmplz_jetpack_stats_markers( $markers ) {
+function cmplz_jetpack_stats_stats_markers( $markers ) {
 	$tags[] = 'pixel.wp.com';
 	$tags[] = 'stats.wp.com';
 	return $markers;
 }
-add_filter( 'cmplz_stats_markers', 'cmplz_jetpack_stats_markers', 20, 1 );
+add_filter( 'cmplz_stats_markers', 'cmplz_jetpack_stats_stats_markers', 20, 1 );
 
 

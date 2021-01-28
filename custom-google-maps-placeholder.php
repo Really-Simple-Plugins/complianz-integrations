0da0e676cb9271738873fff53680af9e3248362e
Aert Hulsebos <39100908+AertHulsebos@users.noreply.github.com>
2021-01-28 11:12:31 +0100

    Update and rename custom-google-maps-placeholder to custom-google-maps-placeholder.php

--- custom-google-maps-placeholder.php
+++ custom-google-maps-placeholder.php
@@ -1,10 +1,12 @@
 <?php
+
 /**
 * URL based on themdirectory and plugin
 * themedir/complianz-gpdr/placeholder-$type.jpg
 * themedir/complianz-gpdr-premium/placeholder-$type.jpg
 * themedir/complianz-gpdr-premium-multisite/placeholder-$type.jpg
 */
+
 defined( 'ABSPATH' ) or die( "you do not have acces to this page!" );
 
 function cmplz_custom_google_maps_placeholder( $url ){

df35132f8a45e65aed1572444125bd3d6a05e3ab
Aert Hulsebos <39100908+AertHulsebos@users.noreply.github.com>
2021-03-17 14:56:42 +0100

    Create exclude-cookie-blocker-and-banner.php
    Client asked for a possibility to pause the banner and blocker based on a UTM parameter. If your campaign URL is appended with '&cmplz_safe_mode', both the blocker and banner asre paused.

---
+++ exclude-cookie-blocker-and-banner.php
@@ -0,0 +1,23 @@
+<?php
+defined( 'ABSPATH' ) or die( "you do not have acces to this page!" );
+
+/**
+ * Filter post types for cookiewarning
+ * It's too early to use global $post or url_to_postid(), so we do a simple text comparison
+ * @param int $cookiewarning_required
+ *
+ * @return bool
+ */
+ function cmplz_my_filter_site_needs_cookiewarning( $cookiewarning_required
+ ) {
+ 	$url = is_ssl() ? "https" : "http" . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+ 	//don't show cookie banner on page where the URL contains the parameter "cmplz_safe_mode"
+ 	if ( strpos($url, 'cmplz_safe_mode') !== FALSE ) {
+ 		$cookiewarning_required = false;
+		//cookie blocker triggers directly on parameter
+ }
+
+ 	return $cookiewarning_required;
+ }
+ add_filter( 'cmplz_site_needs_cookiewarning', 'cmplz_my_filter_site_needs_cookiewarning' );

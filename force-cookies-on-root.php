e1471e6368147255f84292d319440982dcf10261
Aert Hulsebos <39100908+AertHulsebos@users.noreply.github.com>
2021-04-13 15:49:20 +0200

    Update force-cookies-on-root.php

--- force-cookies-on-root.php
+++ force-cookies-on-root.php
@@ -1,6 +1,6 @@
 <?php
 /* Force consent cookies on the root as shown below, or choose a subfolder 
-* Add subfolder between '' on line 6
+* Add subfolder between '' on line 6 e.g. /subfolder/
 */
 function my_cookie_path($path) {
     return '';

e808c05099a3fc07c7b98b218631ce701acc2c79
jarno-vos <78414123+jarno-vos@users.noreply.github.com>
2021-09-02 20:11:11 +0200

    Update exclude-post-type.php

--- exclude-post-type.php
+++ exclude-post-type.php
@@ -1,4 +1,11 @@
 <?php
+defined( 'ABSPATH' ) or die( "you do not have acces to this page!" );
+
+/**
+ * Exclude post types from cookie scan
+ *
+ */
+
 add_filter('cmplz_cookiescan_post_types', 'my_custom_posttypes');
 function my_custom_posttypes($post_types){
 	unset( $post_types['not_this_posttype'] );

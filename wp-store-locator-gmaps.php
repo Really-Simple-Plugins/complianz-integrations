a178fbcb2469284cbc9c6912948b1d3d8f43f06f
jarno-vos <78414123+jarno-vos@users.noreply.github.com>
2021-09-06 20:48:17 +0200

    Update wp-store-locator-gmaps.php

--- wp-store-locator-gmaps.php
+++ wp-store-locator-gmaps.php
@@ -1,19 +1,13 @@
 <?php
-/**
- * There are a lot of custom implementations for Google Maps. These require a custom block script, as described below
- * Replace initMap with a variable from the inline script which runs the init, or the URL where this script resides.
- */
 defined( 'ABSPATH' ) or die( "you do not have acces to this page!" );
-
 /**
- * Block the script, and an inline script with string 'initMap'.
- * initMap can also be something else. That's the problem with custom maps :)
+ * Block the script.
  *
  * @param $tags
  *
  * @return array
  */
-function cmplz_custom_googlemaps_script( $tags ) {
+function cmplz_wpsl_googlemaps_script( $tags ) {
 	$tags[] = 'maps.google.com/maps/api/js';
 	$tags[] = 'wpsl-gmap';
 	$tags[] = 'wpsl-js-js-extra';
@@ -21,30 +15,43 @@ function cmplz_custom_googlemaps_script( $tags ) {
 
 	return $tags;
 }
-add_filter( 'cmplz_known_script_tags', 'cmplz_custom_googlemaps_script' );
+add_filter( 'cmplz_known_script_tags', 'cmplz_wpsl_googlemaps_script' );
 
 /**
  * Conditionally add the dependency
  * $deps['wait-for-this-script'] = 'script-that-should-wait';
  */
 
-function cmplz_custom_maps_dependencies( $tags ) {
-	$tags['maps.google.com/maps/api/js'] = 'wpsl-js-js';
-	$tags['maps.google.com/maps/api/js'] = 'wpsl-js-js-extra';
+function cmplz_wpsl_maps_dependencies( $tags ) {
 	$tags['maps.google.com/maps/api/js'] = 'wpsl-gmap';
 	return $tags;
 }
-add_filter( 'cmplz_dependencies', 'cmplz_custom_maps_dependencies' );
-
+add_filter( 'cmplz_dependencies', 'cmplz_wpsl_maps_dependencies' );
 
 /**
- * Add a placeholder to a div with class "my-maps-class"
+ * Add a placeholder to a div with class "wpsl-gmap-canvas"
  * @param $tags
  *
  * @return mixed
  */
-function cmplz_custom_maps_placeholder( $tags ) {
+function cmplz_wpsl_maps_placeholder( $tags ) {
 	$tags['google-maps'][] = "wpsl-gmap-canvas";
 	return $tags;
 }
-add_filter( 'cmplz_placeholder_markers', 'cmplz_custom_maps_placeholder' );
+add_filter( 'cmplz_placeholder_markers', 'cmplz_wpsl_maps_placeholder' );
+
+/**
+ * Add services to the list of detected items, so it will get set as default, and will be added to the notice about it
+ *
+ * @param $services
+ *
+ * @return array
+ */
+function cmplz_wp_google_maps_detected_services( $services ) {
+	if ( ! in_array( 'google-maps', $services ) ) {
+		$services[] = 'google-maps';
+	}
+
+	return $services;
+}
+add_filter( 'cmplz_detected_services', 'cmplz_wp_google_maps_detected_services' );

85c44471a373b549a6609a6d7622ae9d13e05e07
Aert Hulsebos <39100908+AertHulsebos@users.noreply.github.com>
2021-11-05 17:03:30 +0100

    Update exclude-cookie-warning.php

--- exclude-cookie-warning.php
+++ exclude-cookie-warning.php
@@ -13,7 +13,11 @@ function cmplz_my_filter_site_needs_cookiewarning( $cookiewarning_required
 	$url = is_ssl() ? "https" : "http" . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
 	//don't show cookie banner on page where the URL contains the word "shop"
-	if ( strpos($url, 'shop') !== FALSE ) {
+	if ( strpos($url, 'shop') !== FALSE )
+	// don't show on specific Post ID's - uncomment below	
+	// if ($post->ID==1 || $post->ID==2 || $post->ID==3) 
+
+	{
 		$cookiewarning_required = false;
 
 		//to disable the cookieblocker, uncomment the following

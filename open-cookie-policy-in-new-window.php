d56334925765d0aaca64dc85bf758ca59c9f1030
jarno-vos <jarno@really-simple-plugins.com>
2021-12-07 15:24:25 +0100

    remove DOMContentLoaded

--- open-cookie-policy-in-new-window.php
+++ open-cookie-policy-in-new-window.php
@@ -6,7 +6,6 @@
 function cmplz_open_in_new_tab() {
     ?>
     <script>
-        document.addEventListener("DOMContentLoaded", function() {
             document.addEventListener('cmplz_cookie_warning_loaded', function (e) {
                 var links = document.getElementsByClassName('cmplz-link');
                 var len = links.length;
@@ -14,9 +13,7 @@ function cmplz_open_in_new_tab() {
                 for(var i=0; i<len; i++) {
                     links[i].setAttribute('target', '_blank');
                 }
-
         });
-    });
     </script>
     <?php
 }

14256998d5e81b4d2b397a4074e21b4f0bf4b26e
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-01-17 17:13:55 +0100

    obfuscator

---
+++ obfuscate-email-shortcode.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Shortcode to use the Complianz email obfuscator
+ * Usage: [cmplz_obfuscate email="john@doe.com"]
+ * @param $atts
+ * @param $content
+ * @param $tag
+ *
+ * @return false|string
+ */
+function cmplz_obfuscate_shortcode(
+	$atts = array(), $content = null, $tag = ''
+) {
+	$atts   = shortcode_atts( array(
+		'email'   => '',
+	), $atts, $tag );
+	$email = sanitize_email($atts['email']);
+	ob_start();
+	echo COMPLIANZ::$document->obfuscate_email($email);
+	return ob_get_clean();
+}
+add_shortcode( 'cmplz_obfuscate', 'cmplz_obfuscate_shortcode' );

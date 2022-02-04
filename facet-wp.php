c568bf4f996b5df1c8745e4b196c921a4234cae4
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-02-04 13:51:37 +0100

    Facet WP integration

---
+++ facet-wp.php
@@ -0,0 +1,75 @@
+<?php
+defined( 'ABSPATH' ) or die();
+/**
+ * props @Idan-noiza
+ * https://github.com/Really-Simple-Plugins/complianz-gdpr/issues/348
+ */
+
+/**
+ * Add a script to the blocked list
+ * @param array $tags
+ *
+ * @return array
+ */
+function cmplz_facetwp_script( $tags ) {
+	$wait_for = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? 'facetwp-map-facet/assets/js/front.js' : 'facetwp/assets/js/dist/front.min.js';
+	$tags[] = array(
+		'name' => 'FacetWP',
+		'category' => 'marketing',
+		'urls' => array(
+			'maps.googleapis.com',
+			'facetwp/assets/js/dist/front.min.js',
+			'facetwp-map-facet/assets/js/front.js',
+		),
+		'enable_placeholder' => '1',
+		'placeholder' => 'google-maps',
+		'placeholder_class' => 'custom-css-class-for-embed-google-map',
+		'enable_dependency' => '1',
+		'dependency' => [
+			//'wait-for-this-script' => 'script-that-should-wait'. This should reflect the order in which it should load.
+			'maps.googleapis.com' => $wait_for,
+		],
+	);
+
+	return $tags;
+}
+add_filter( 'cmplz_known_script_tags', 'cmplz_facetwp_script' );
+/**
+ * Re Initialize Map
+ *
+ */
+
+function cmplz_facetwp_map_initDomContentLoaded() {
+	ob_start();
+	?>
+	<script>
+        function sleep(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
+
+        const checkIfFWPExists = async () => {
+            if (typeof FWP !== 'undefined' && typeof FWP_MAP !== 'undefined' && typeof FWP_MAP.map !== 'undefined') {
+                return true;
+            }
+            await sleep(1000);
+            checkIfFWPExists();
+        }
+
+        const fixMap = async (event) => {
+            if (event.detail.category === "marketing") {
+                await checkIfFWPExists();
+                FWP.loaded = false;
+                FWP.refresh();
+                document.removeEventListener('cmplz_enable_category', fixMap);
+            }
+        }
+        document.addEventListener('cmplz_enable_category', fixMap);
+	</script>
+	<?php
+	$script = ob_get_clean();
+	$script = str_replace(array('<script>', '</script>'), '', $script);
+	wp_add_inline_script( 'cmplz-cookiebanner', $script );
+}
+add_action( 'wp_enqueue_scripts', 'cmplz_facetwp_map_initDomContentLoaded',PHP_INT_MAX );
+
+

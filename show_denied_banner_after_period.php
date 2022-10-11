0a2d5847b39ea4a6349cb5da8a0f588aa9a78876
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-10-11 14:51:41 +0200

    denied banner code

--- show_denied_banner_after_period.php
+++ show_denied_banner_after_period.php
@@ -1,49 +1,66 @@
 <?php
 function cmplz_set_denied_time_cookie() {
-    ob_start(); ?>
+	ob_start(); ?>
     <script>
         let cmplz_cookie_name = 'denied_time';
-
-        document.addEventListener("cmplz_banner_status", function(consentData) {
-            var status = consentData.detail;
-            if ( status === 'dismissed' ) {
-                let daysValid = 365;
-                let secure = ";secure";
-                let date = new Date();
-                date.setTime(date.getTime() + (daysValid * 24 * 60 * 60 * 1000));
-                let expires = ";expires=" + date.toGMTString();
-                let value = date.getTime();
-                if (window.location.protocol !== "https:") secure = '';
-                document.cookie = cmplz_cookie_name + "=" + value + ";SameSite=Lax" + secure + expires;
-            }
-        });
-
-        document.addEventListener("cmplz_cookie_warning_loaded", function() {
-            cmplz_cookie_name = cmplz_cookie_name + "=";
+        function get_denied_time(){
+            let cookie_name = cmplz_cookie_name + "=";
             let cArr = document.cookie.split(';');
-            let denied_time;
+            let denied_time=false;
             for (let i = 0; i < cArr.length; i++) {
                 let c = cArr[i].trim();
-                console.log("check cookie "+c);
-                if (c.indexOf(cmplz_cookie_name) == 0)
-                    denied_time = c.substring(cmplz_cookie_name.length, c.length);
+                if (c.indexOf(cookie_name) == 0)
+                    denied_time = c.substring(cookie_name.length, c.length);
             }
-            console.log("found time "+denied_time);
+            return denied_time;
+        }
 
+        function set_denied_cookie(){
+            let daysValid = 365;
+
+            let secure = ";secure";
             let date = new Date();
-            let current_time = date.getTime();
-            let expire_on = parseInt(denied_time) + (7 * 24 * 60 * 60 );
-            console.log("expire on "+expire_on);
-            if ( current_time > expire_on ){
-                console.log("expired");
-                cmplz_set_banner_status('show');
+            // let ask_again_on = date.setTime(date.getTime()) + (24 * 60 * 60 * 1000);
+            //20 seconds
+            let ask_again_on = date.setTime(date.getTime()) + (60 * 1000);
+
+            date.setTime(date.getTime() + (daysValid * 24 * 60 * 60 * 1000));
+            let expires = ";expires=" + date.toGMTString();
+            if (window.location.protocol !== "https:") secure = '';
+            document.cookie = cmplz_cookie_name + "=" + ask_again_on + ";SameSite=Lax" + secure + expires;
+        }
+
+
+        document.addEventListener("cmplz_cookie_warning_loaded", function() {
+            let denied_time = get_denied_time();
+            console.log(denied_time);
+            if ( !denied_time && cmplz_get_banner_status() === 'dismissed' ) {
+                set_denied_cookie();
+            } else {
+                console.log("found time "+denied_time);
+                let date = new Date();
+                let current_time = date.getTime();
+
+                // let expire_on = parseInt(denied_time) + (7 * 24 * 60 * 60 * 1000 );
+                let expire_on = parseInt(denied_time);
+                console.log("current time "+current_time);
+                console.log("expire on "+expire_on);
+                console.log(expire_on - current_time);
+                if ( current_time > expire_on ){
+                    console.log("expired");
+                    cmplz_set_banner_status('show');
+                    set_denied_cookie();
+                } else {
+                    console.log("not expired");
+                }
             }
+
         });
 
     </script>
-    <?php
-    $script = ob_get_clean();
-    $script = str_replace(array('<script>', '</script>'), '', $script);
-    wp_add_inline_script( 'cmplz-cookiebanner', $script);
+	<?php
+	$script = ob_get_clean();
+	$script = str_replace(array('<script>', '</script>'), '', $script);
+	wp_add_inline_script( 'cmplz-cookiebanner', $script);
 }
 add_action( 'wp_enqueue_scripts', 'cmplz_set_denied_time_cookie', PHP_INT_MAX );

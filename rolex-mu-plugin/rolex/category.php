e3efc659a327f64625d0d1ee11967adc5cfbe9bb
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-11-09 13:17:54 +0100

    adjust rolex integrations

---
+++ rolex-mu-plugin/rolex/category.php
@@ -0,0 +1,22 @@
+<details class="cmplz-category cmplz-rlx" >
+	<summary>
+			<span class="cmplz-category-header">
+				<span class="cmplz-category-title"><?php _e("RLX category name", "complianz-gdpr")?></span>
+				<span class="cmplz-banner-checkbox">
+					<input type="checkbox"
+					       id="cmplz-rlx-optin"
+					       data-category="cmplz_rlx"
+					       class="cmplz-consent-checkbox cmplz-rlx"
+					       size="40"
+					       value="1"/>
+					<label class="cmplz-label" for="cmplz-rlx-optin" tabindex="0"><span><?php _e("RLX label", "complianz-gdpr")?></span></label>
+				</span>
+				<span class="cmplz-icon cmplz-open">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"  height="18" ><path d="M224 416c-8.188 0-16.38-3.125-22.62-9.375l-192-192c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L224 338.8l169.4-169.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-192 192C240.4 412.9 232.2 416 224 416z"/></svg>
+				</span>
+			</span>
+	</summary>
+	<div class="cmplz-description">
+		<span class="cmplz-description-rlx"><?php _e("RLX description", "complianz-gdpr")?></span>
+	</div>
+</details>

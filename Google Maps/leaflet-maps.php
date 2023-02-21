11f2a597f5710689335e020b31bc57e99215608b
jarno-vos <78414123+jarno-vos@users.noreply.github.com>
2023-02-21 22:11:44 +0100

    remove dependency

--- Google Maps/leaflet-maps.php
+++ Google Maps/leaflet-maps.php
@@ -1,7 +1,6 @@
 <?php
-defined( 'ABSPATH' ) or die( "you do not have acces to this page!" );
+defined( 'ABSPATH' ) or die( "you do not have access to this page!" );
 
-add_filter( 'cmplz_known_script_tags', 'cmplz_leafletmaps_directory_script' );
 function cmplz_leafletmaps_directory_script( $tags ) {
 	$tags[] = array(
 		'name' => 'openstreetmaps',
@@ -12,29 +11,27 @@ function cmplz_leafletmaps_directory_script( $tags ) {
 		),
 		'enable_placeholder' => '1',
 		'placeholder_class' => 'leaflet-map',
-		'enable_dependency' => '1',
+		'enable_dependency' => '0',
 		'dependency' => [
 			//'wait-for-this-script' => 'script-that-should-wait'
-			'leaflet.js' => 'leaflet-gesture-handling-leafext.min.js',
-		],
-	);
-	return $tags;
-}
-
-/**
- * Add services to the list of detected items, so it will get set as default, and will be added to the notice about it
- *
- * @param $services
- *
- * @return array
- */
-
-function cmplz_leafletmaps_directory_detected_services( $services ) {
-	if ( ! in_array( 'openstreetmaps', $services ) ) {
-		$services[] = 'openstreetmaps';
+			],
+		);
+		return $tags;
 	}
+	add_filter( 'cmplz_known_script_tags', 'cmplz_leafletmaps_directory_script' );
 
-	return $services;
-}
+	/**
+	* Add services to the list of detected items, so it will get set as default, and will be added to the notice about it
+	*
+	* @param $services
+	*
+	* @return array
+	*/
 
-add_filter( 'cmplz_detected_services', 'cmplz_leafletmaps_directory_detected_services' );
+	function cmplz_leafletmaps_directory_detected_services( $services ) {
+		if ( ! in_array( 'openstreetmaps', $services ) ) {
+			$services[] = 'openstreetmaps';
+		}
+		return $services;
+	}
+	add_filter( 'cmplz_detected_services', 'cmplz_leafletmaps_directory_detected_services' );

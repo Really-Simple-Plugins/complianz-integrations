751ba9b162c9f668bf998d0701f6aa3cd65d9e69
Aert Hulsebos <39100908+AertHulsebos@users.noreply.github.com>
2023-08-11 15:20:50 +0200

    Update gtm-consent-mode.js

--- gtm-consent-mode.js
+++ gtm-consent-mode.js
@@ -1,3 +1,4 @@
+<script>
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('consent', 'default', {
@@ -42,11 +43,11 @@ document.addEventListener("cmplz_fire_categories", function (e) {
 	});
 });
 
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','{GTM_code}');
+// (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+// 		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+// 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+// 	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+// })(window,document,'script','dataLayer','{GTM_code}');
 
 document.addEventListener("cmplz_revoke", function (e) {
 	gtag('consent', 'update', {
@@ -57,3 +58,4 @@ document.addEventListener("cmplz_revoke", function (e) {
 		'ad_storage': "denied",
 	});
 });
+</script>

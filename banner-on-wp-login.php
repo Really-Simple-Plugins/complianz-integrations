d9efa2d040f219e8eed4019fe2ac8ba0d6b3935b
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-11-15 15:09:11 +0100

    function name

--- banner-on-wp-login.php
+++ banner-on-wp-login.php
@@ -1,7 +1,7 @@
 <?php
-function cmplz_load_wizard_shortcode(){
+function cmplz_my_add_banner(){
 	add_action( 'login_enqueue_scripts', array(COMPLIANZ::$banner_loader, 'enqueue_assets') );
 	add_action( 'login_head', array( COMPLIANZ::$banner_loader, 'cookiebanner_css' ) );
 	add_action( 'login_footer', array( COMPLIANZ::$banner_loader, 'cookiebanner_html' ) );
 }
-add_action('plugins_loaded', 'cmplz_load_wizard_shortcode');
+add_action('plugins_loaded', 'cmplz_my_add_banner');
